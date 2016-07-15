e618d5098e0e79a8881ecd08cdb9613c9ff5dbf1
bas080 <bas080@hotmail.com>
2016-07-15 19:05:01 +0200

    uses the git directory name as project

--- bash-wakatime.sh
+++ bash-wakatime.sh
@@ -6,14 +6,19 @@
 # or this command:
 #   source path/to/bash-wakatime.sh
 #
-# But if you already have a PROMPT_COMMAND variable set,
-# just merge yout own pre_prompt_command with the following one.
-# And don't forget to create and configure your "~/.wakatime.cfg" file.
-#
+# Don't forget to create and configure your "~/.wakatime.cfg" file.
 
 # hook function to send wakatime a tick
 pre_prompt_command() {
-    (wakatime --write --plugin "bash-wakatime/0.0.1" --entity-type app --project Terminal --entity "$(echo $(fc -ln -0) | cut -d ' ' -f1)" 2>&1 > /dev/null &)
+
+    local project=$( \
+        type git                               2>&1 > /dev/null && \
+        git rev-parse --git-dir > /dev/null    2>&1 > /dev/null && \
+        basename git rev-parse --show-toplevel 2>&1 > /dev/null || \
+        echo terminal
+    )
+
+    (wakatime --write --plugin "bash-wakatime/0.0.1" --entity-type app --project $project --entity "$(echo $(fc -ln -0) | cut -d ' ' -f1)" 2>&1 > /dev/null &)
 }
 
 PROMPT_COMMAND="pre_prompt_command; $PROMPT_COMMAND"
