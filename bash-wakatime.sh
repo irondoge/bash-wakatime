01c4e55ae1a62749ea07f9cc66202115274b2912
Alexandr Shavykin <el.nprotect@gmail.com>
2023-09-10 16:26:27 +0300

    Update bash-wakatime.sh

--- bash-wakatime.sh
+++ bash-wakatime.sh
@@ -13,8 +13,8 @@ pre_prompt_command() {
     version="1.0.0"
     entity=$(echo $(fc -ln -0) | cut -d ' ' -f1)
     [ -z "$entity" ] && return # $entity is empty or only whitespace
-    $(git rev-parse --is-inside-work-tree 2> /dev/null) && local project="$(basename $(git rev-parse --show-toplevel))" || local project="Terminal"
-    (~/.wakatime/wakatime-cli --write --plugin "bash-wakatime/$version" --entity-type app --project "$project" --entity "$entity" 2>&1 > /dev/null &)
+    git rev-parse --is-inside-work-tree &> /dev/null && local project="$(basename "$(git rev-parse --show-toplevel)")" || local project="Terminal"
+    (~/.wakatime/wakatime-cli --write --plugin "bash-wakatime/$version" --entity-type app --project "$project" --entity "$entity" &> /dev/null &)
 }
 
 PROMPT_COMMAND="pre_prompt_command; $PROMPT_COMMAND"
